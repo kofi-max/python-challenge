cfdd14a601318a23a81dc49d7234c1eef96f2ec8
Maxwell Ansah <maxx.ansah@gmail.com>
2021-10-01 00:32:57 +0100

    none

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,7 +1,7 @@
 # CSV data is loaded which contains two columns: Date and Profit/Losses
 import csv
 import os
-csvpath = os.path.join('/Resources/budget_data.csv')
+csvpath = os.path.join('C:\Users\User\Desktop\ubhm-brm-data-pt-06-2021-u-c\Homeworks\03-Python\Instructions\PyBank\Resources\budget_data.cvs')
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     header_row = next(csvreader)
